d70afe36d3399a5d43611037a7df8a5edd5d79a4
ervanervan <ervanervan040@gmail.com>
2024-07-13 22:39:24 +0700

    install: react-syntax-highlighter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-gray-200">
+        <div className="container mx-auto">
+          <div className="grid place-items-center h-screen">Hello world!</div>
+        </div>
+      </div>
     </>
   );
 }
